67da17243a43cbf8237e87221d79ee3b6d69253f
Harald Vinje <haraldvinje@gmail.com>
2020-12-01 12:55:20 +0100

    init commit

---
+++ crop_image_object.py
@@ -0,0 +1,40 @@
+from lib.aws_rekognition.rekognition_image_detection import get_bouding_box_of_object
+from PIL import Image
+from pathlib import Path
+import uuid
+from sys import argv
+
+
+def crop_PIL_image_to_bounding_box(filename):
+    image = Image.open(filename)
+
+    bounding_box = get_bouding_box_of_object(image_file_name=filename)
+    left = bounding_box['Left']
+    top = bounding_box['Top']
+    height = bounding_box['Height']
+    width = bounding_box['Width']
+
+    image_width, image_height = image.size
+
+    x0 = image_width*left
+    y0 = image_height*top
+    x1 = x0 + image_width*width
+    y1 = y0 + image_height*height
+
+    cropped = image.crop((x0, y0, x1, y1))
+    return cropped
+
+
+def save_PIL_to_file(PIL_image, filename):
+    file_exists = Path(filename)
+    new_filename = filename
+    if file_exists.is_file():
+        name, extension = new_filename.split('.')
+        new_filename = name + str(uuid.uuid4()) + "." + extension
+    PIL_image.save(new_filename)
+
+if __name__=='__main__':
+    filename = argv[1]
+    cropped_image = crop_PIL_image_to_bounding_box(filename)
+    filename_cropped = "cropped_" + filename
+    save_PIL_to_file(cropped_image, filename_cropped)
